f2dc625cf1dd95dc0af2537f0ea24ec39623dc9f
CrafterPika <48294025+CrafterPika@users.noreply.github.com>
2021-06-05 18:41:40 +0200

    alol

---
+++ res/plg_mgr_functions.js
@@ -0,0 +1,8 @@
+async function rmPluginID(json_id) {
+    var newJSONMeta = await pluginmeta.splice(json_id, json_id);
+    console.log(newJSONMeta)
+    await localStorage.removeItem("plugins");
+    await localStorage.setItem('plugins', JSON.stringify(newJSONMeta, 2));
+    alert('Plugin should be uninstalled')
+    location.reload()
+}
