a9cec49e9886315caa0984c9f597bdb60da7027c
CrafterPika <48294025+CrafterPika@users.noreply.github.com>
2021-06-05 17:03:13 +0200

    YES

--- res/rmPluginID.js
+++
@@ -1,8 +0,0 @@
-var localstorplugins = localStorage.getItem('plugins');
-var pluginmeta = JSON.parse(localstorplugins);
-async function rmPluginID(id) {
-    pluginmeta.splice(id, 1);
-    localStorage.removeItem("plugins");
-    localStorage.setItem('plugins', pluginmeta);
-    alert('Plugin should be uninstalled')
-}
