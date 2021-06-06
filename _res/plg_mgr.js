5639ce30cf4aa2256655c6a80a2b07d09f0434a1
CrafterPika <48294025+CrafterPika@users.noreply.github.com>
2021-06-06 12:19:08 +0200

    pog

--- _res/plg_mgr.js
+++ _res/plg_mgr.js
@@ -2,9 +2,16 @@
 var localstorplugins = localStorage.getItem('plugins');
 var pluginmeta = JSON.parse(localstorplugins);
 
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+    var rest = this.slice((to || from) + 1 || this.length);
+    this.length = from < 0 ? this.length + from : from;
+    return this.push.apply(this, rest);
+};
+
+// Remove function
 async function rmPluginID(index_name) {
-    var pluginIndex = pluginmeta.indexOf(index_name);
-    pluginmeta.splice(pluginIndex, 1);
+    await pluginmeta.remove(index_name);
     await localStorage.removeItem("plugins");
     await localStorage.setItem('plugins', JSON.stringify(pluginmeta, 2));
     alert('Plugin should be uninstalled')
